64f430b370e2b30fb78d969418ec5749d7b5aeb0
Bharat Paliwal <18uec169@lnmiit.ac.in>
2023-11-24 02:11:58 +0530

    lazy loading impl

--- src/App.jsx
+++ src/App.jsx
@@ -5,22 +5,24 @@ import { BrowserRouter as Router,Redirect,Route,Switch} from 'react-router-dom';
 //css
 import {Container} from '@material-ui/core'
 
-//pages
-import Auth from './pages/Auth'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import PostDetail from './pages/PostDetails'
-import Tags from './pages/Tags'
-import EmailVerification from './pages/EmailVerification';
-import ChangePassword from './pages/ChangePassword';
-import ForgotPassword from './pages/ForgotPassword';
-import Verification from './pages/EmailVerification/Verified'
-//components
-import Navbar from './components/NavbarUI'
-import Footer from './components/Footer'
 
-import BTP from './utils/BTP'
 import Loading from './utils/FlashUI'
+
+//pages
+const Home = React.lazy(()=> import("./pages/Home"));
+const Auth = React.lazy(()=> import("./pages/Auth"));
+const Profile = React.lazy(()=> import("./pages/Profile"));
+const PostDetail = React.lazy(()=> import("./pages/PostDetails"));
+const Tags = React.lazy(()=> import("./pages/Tags"));
+const EmailVerification = React.lazy(()=> import("./pages/EmailVerification"));
+const Verification = React.lazy(()=>import("./pages/EmailVerification/Verified"));
+const ChangePassword = React.lazy(()=>import("./pages/ChangePassword"));
+const ForgotPassword = React.lazy(()=>import("./pages/ForgotPassword"));
+
+//components
+const Navbar = React.lazy(()=> import("./components/NavbarUI"));
+const Footer = React.lazy(()=> import("./components/Footer"));
+const BTP = React.lazy(()=> import("./utils/BTP"));
 
 //__init__
 const App = () => {
@@ -34,29 +36,32 @@ const App = () => {
   
   const user = JSON.parse(localStorage.getItem('profile'));
   if(load){
-    return <Loading></Loading>
+    return <Loading />
   }
   return (
     <>
       <Router>
         <Container maxwidth="xl">
-          <Navbar></Navbar>
+          <React.Suspense fallback={<h1>MemoFeed</h1>}>
+            <Navbar />
+          </React.Suspense>
+
+          <React.Suspense fallback={<Loading />}>
             <Switch>
               <Route path="/" exact component={() => <Redirect to="/posts" />} ></Route>
-              <Route path="/posts" exact component={Home}></Route>
-              <Route path="/posts/search" exact component = {Home} ></Route>
-              <Route path="/posts/:id" exact component = {PostDetail}></Route>
-              <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)}></Route>
-              <Route path="/profile" exact component = {Profile} ></Route>
-              <Route path="/tags/:name" exact component = {Tags} ></Route>
-              <Route path="/auth/changepassword" exact component ={ChangePassword}></Route>
-              <Route path="/auth/forgotpassword" exact component={ForgotPassword}></Route>
+              <Route path="/posts" exact component={props => <Home {...props} />}></Route>
+              <Route path="/posts/search" exact component = {props => <Home {...props} />} ></Route>
+              <Route path="/posts/:id" exact component = {props => <PostDetail {...props} />}></Route>
+              <Route path="/auth" exact component={() => (!user ? (props=> <Auth {...props} />) : <Redirect to="/posts" />)}></Route>
+              <Route path="/profile" exact component = {props => <Profile {...props} />} ></Route>
+              <Route path="/tags/:name" exact component = {props => <Tags {...props}/>} ></Route>
+              <Route path="/auth/changepassword" exact component ={props=> <ChangePassword {...props} />}></Route>
+              <Route path="/auth/forgotpassword" exact component={props=> <ForgotPassword {...props} />}></Route>
               {/* <Route path="/auth/emailverification" exact component={() => (user ? <EmailVerification /> : <Redirect to="/posts" />)}></Route> */}
-              <Route path="/auth/emailverification" exact component={EmailVerification}></Route>
-              <Route path="/auth/verification" exact component={() => (user ? <Verification /> : <Redirect to="/posts" />)} ></Route>
+              <Route path="/auth/emailverification" exact component={props=> <EmailVerification {...props}/>}></Route>
+              <Route path="/auth/verification" exact component={() => (user ? (props=> <Verification {...props} />) : <Redirect to="/posts" />)} ></Route>
               <Route path="*" component={() =><Redirect to="/posts" />} ></Route>
             </Switch>
-          <React.Suspense fallback={<Loading/>}>
             <BTP />
             <Footer />
           </React.Suspense>  
